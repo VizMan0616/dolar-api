94ae8d15caf2ee3e0a68a754068308566b4c9b5a
Viz0616 <josevizcaya0616@gmail.com>
2021-06-20 03:49:27 -0400

    Revert "MID-#0007 Deploy errors"
    This reverts commit ca1964a3d89955016deec232f90346bfe4bc5f02.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-
+from flask import Flask
 #from flask_sqlalchemy import SQLAlchemy
 #from .common import Config
 
@@ -6,12 +6,13 @@
 
 
 def create_app():
-    
+    app = Flask(__name__)
 #    app.config.from_object(Config)
 
 #    api_db.init_app(app)
 
-    
+    from .routes import api_bp
+    app.register_blueprint(api_bp)
 
     return app
 
--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 from app import create_app
-from flask import Flask
-
-app = Flask(__name__)
-
-from app.routes import api_bp
-app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
