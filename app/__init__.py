ca1964a3d89955016deec232f90346bfe4bc5f02
Viz0616 <josevizcaya0616@gmail.com>
2021-06-20 03:40:03 -0400

    MID-#0007 Deploy errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+
 #from flask_sqlalchemy import SQLAlchemy
 #from .common import Config
 
@@ -6,13 +6,12 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    
 #    app.config.from_object(Config)
 
 #    api_db.init_app(app)
 
-    from .routes import api_bp
-    app.register_blueprint(api_bp)
+    
 
     return app
 
