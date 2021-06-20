dd8a41f523a34431ed6193129b501525f2253a8f
Viz0616 <josevizcaya0616@gmail.com>
2021-06-20 13:52:35 -0400

    MID-#0011 Added cors policy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 #from flask_sqlalchemy import SQLAlchemy
 #from .common import Config
 
 #api_db = SQLAlchemy()
-
+cors = CORS()
 
 def create_app():
     app = Flask(__name__)
+    cors.init_app(app)
 #    app.config.from_object(Config)
 
 #    api_db.init_app(app)
