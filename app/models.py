0a7231038644b90777fa0eb677250daa25d2f448
VizMan0616 <60019108+VizMan0616@users.noreply.github.com>
2021-06-20 03:12:43 -0400

    MID-#0007 Modified time format

--- app/models.py
+++ app/models.py
@@ -5,5 +5,5 @@ class DollarModel:
     def __init__(self, dollar, bolivar):
         self.dollar_quantity = dollar
         self.bolivar_quantity = bolivar
-        self.date_asked = datetime.now()
+        self.date_asked = datetime.utcnow()
 
